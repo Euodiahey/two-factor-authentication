f7e92b62c0c5e2dac6503c7b9aeae0d881ef0e04
birch <121175143+birchrust@users.noreply.github.com>
2023-07-19 16:20:51 +0800

    Connect to the database

--- src/server/api/routers/totp.ts
+++ src/server/api/routers/totp.ts
@@ -5,4 +5,30 @@ export const totpRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.totp.findMany();
   }),
+  add: publicProcedure
+    .input(
+      z.object({
+        issuer: z.string(),
+        label: z.string(),
+        algorithm: z.string(),
+        digits: z.number(),
+        period: z.number(),
+        secret: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const totp = await ctx.prisma.totp.create({
+        data: input,
+      });
+      return totp;
+    }),
+  del: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.totp.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
 });
